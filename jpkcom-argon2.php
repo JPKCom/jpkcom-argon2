bcd5e00f9ca0885074b6cd811f4db0306004e4fa
Jean Pierre Kolb <jp.kolb@creationell.de>
2025-02-25 08:57:20 +0100

    v1.0.4

--- jpkcom-argon2.php
+++ jpkcom-argon2.php
@@ -3,11 +3,12 @@
 Plugin Name: JPKCom Enable Argon2
 Plugin URI: https://github.com/JPKCom/jpkcom-argon2
 Description: Enables ARGON2ID for password hashes.
-Version: 1.0.3
+Version: 1.0.4
 Author: Jean Pierre Kolb <jpk@jpkc.com>
 Author URI: https://www.jpkc.com
 Requires at least: 6.8
 Requires PHP: 8.3
+Network: true
 License: MIT
 License URI: https://opensource.org/license/MIT
 GitHub Plugin URI: JPKCom/jpkcom-argon2
